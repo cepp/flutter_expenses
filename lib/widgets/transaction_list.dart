fc1184716cfd606fa7483944313c3092140df215
carlos <ceppantoja@gmail.com>
2020-03-17 13:31:44 -0300

    adding delete labeled button

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -58,11 +58,20 @@ class TransactionList extends StatelessWidget {
                   subtitle: Text(
                     DateFormat.yMMMd().format(transaction.date),
                   ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    color: Theme.of(context).errorColor,
-                    onPressed: () => this._deleteTransaction(transaction.id),
-                  ),
+                  trailing: MediaQuery.of(context).size.width > 460
+                      ? FlatButton.icon(
+                          icon: Icon(Icons.delete),
+                          label: Text('Delete'),
+                          textColor: Theme.of(context).errorColor,
+                          onPressed: () =>
+                              this._deleteTransaction(transaction.id),
+                        )
+                      : IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () =>
+                              this._deleteTransaction(transaction.id),
+                        ),
                 ),
               );
             },
